2735ae79ac3a104201973323e295339f7c839d7f
Yamila <yamilarobles10@gmail.com>
2024-02-21 21:20:06 +0100

    Add cardInfoJunior

--- client/src/components/cardInfoJunior/CardInfoJunior.jsx
+++ client/src/components/cardInfoJunior/CardInfoJunior.jsx
@@ -1,31 +1,32 @@
-import './cardInfoJunior.css';
-import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
+import "./cardInfoJunior.css";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 function CardInfoJunior() {
-
-    return (
-        <>
-         <Container>
-                <Row>
-                    <Col>
-                        <Card body className="d-flex align-items-center">
-                          <Card.Img src="holder.js/171x180" roundedCircle />
-                          
-                            <Card.Title>Nombre: Juan Pérez</Card.Title>
-                            <Card.Subtitle>Experiencia: 2 años</Card.Subtitle>
-                            <Card.Text>Stack: React, Node, MongoDB</Card.Text>
-                          
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    )
-
+  return (
+    <>
+      <Container className="d-flex justify-content-center ">
+        <Row>
+          <Col>
+            <Card className="border-0" style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="https://i.postimg.cc/7YhThmnX/junior1.png" />
+              <Card.Body>
+              <Card.Title className="fw-bold custom-text-color">NOMBRE</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default CardInfoJunior;
