7ceb9089a3fa28aab295d090484948a4414e613f
joyjos <enjoyjos@hotmail.com>
2024-02-22 19:02:15 +0100

    Tests added

---
+++ client/src/tests/NavbarCustom.test.jsx
@@ -0,0 +1,25 @@
+import { test, expect } from "vitest";
+import NavbarCustom from '../components/navbar/NavbarCustom';
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from 'react-router-dom';
+
+test("El componente NavbarCustom se renderiza correctamente", () => {
+  // Renderiza el componente dentro de un BrowserRouter
+  render(
+    <BrowserRouter>
+      <NavbarCustom />
+    </BrowserRouter>
+  );
+
+  // Verifica la existencia del logo de la empresa
+  expect(screen.getByAltText("company logo")).toBeInTheDocument();
+
+  // Verifica la existencia de elementos de navegación
+  expect(screen.getByText("Junior")).toBeInTheDocument();
+  expect(screen.getByText("Tus Junior")).toBeInTheDocument();
+  expect(screen.getByText("Contáctame")).toBeInTheDocument();
+  expect(screen.getByText("Trabaja con nosotros")).toBeInTheDocument();
+
+  // Verifica la existencia del logo de usuario
+  expect(screen.getByAltText("user logo")).toBeInTheDocument();
+});
